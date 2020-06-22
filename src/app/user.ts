056101fa74e536ed5ff4ee858a350c6498223798
musyoka8 <nagilmusyoka@gmail.com>
2020-06-22 22:49:51 +0300

    add additional paramaters

--- src/app/user.ts
+++ src/app/user.ts
@@ -1,5 +1,15 @@
 export class User {
-    constructor(public name: string, public username:string, public followers:number, public following: number, public repositories:number, public user, public profile:string, public date: Date){
+    constructor(
+        public avatar_url:any,
+        public login:any,
+        public company: any,
+        public location: any,
+        public  name: any,
+        public followers: any,
+        public following: any,
+        public created_at: Date,
+        public public_repos: number,
+        public html_url: any,
+      ){}
 
-    }
 }
