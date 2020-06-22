4e0018708bc118d659beae91735f22d607419e0d
musyoka8 <nagilmusyoka@gmail.com>
2020-06-22 12:47:07 +0300

    add paths to the home component

--- src/app/profile-service/profile.service.ts
+++ src/app/profile-service/profile.service.ts
@@ -2,11 +2,13 @@ import { Injectable } from '@angular/core';
 import { User } from '../user';
 import { Repository } from '../repository';
 import { HttpClient } from '@angular/common/http';
+import { environment} from '../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
+  [x: string]: ApiResponse;
   user:User;
   repo:Repository;
 
@@ -15,64 +17,65 @@ export class ProfileService {
     this.repo = new Repository ("", "", "", new Date());
    }
 
- // 
-  // searchUsers(searchTerm:string){
-  //   console.log(searchTerm)
-  //   interface ApiResponse {
-  //   name:string;
-  //   username:string;
-  //   avatarUrl:string;
-  //   followers:number;
-  //   following:number;
-  //   repositories: number;
-  //   profileUrl: string;
-  //   date:Date;
-  //   }
-  //   let searchPoint = 'https://api.github.com/users/' + searchTerm + '?access_token=' + environment.GITAPIKEY;
-  //   console.log(searchPoint);
+ 
+  searchUsers(searchTerm:string){
+    console.log(searchTerm)
+    interface ApiResponse {
+    name:string;
+    username:string;
+    avatarUrl:string;
+    followers:number;
+    following:number;
+    repositories: number;
+    profileUrl: string;
+    date:Date;
+    }
+    let searchPoint = 'https://api.github.com/users/' + searchTerm + '?access_token=' + environment.GITAPIKEY;
+    console.log(searchPoint);
 
-  //   let promise = new Promise((resolve, reject) => {
+    let promise = new Promise((resolve, reject) => {
 
-  //     this.http.get<ApiResponse>(searchPoint).toPromise().then(
-  //       (results) => {
+      this.http.get<ApiResponse>(searchPoint).toPromise().then(
+        (results) => {
 
-  //         console.log(results);
-  //         this.user = results;
-  //         console.log(this.user);
+          console.log(results);
+          this.user = results;
+          console.log(this.user);
 
-  //         resolve();
-  //       },
-  //       (error) => {
-  //         console.log(error);
-  //         reject();
-  //       }
-  //     );
-  //   });
-  //   return promise;
-  // }
+          resolve();
+        },
+        (error) => {
+          console.log(error);
+          reject();
+        }
+      );
+    });
+    return promise;
+  }
 
-  // getRepos(searchTerm) {
+  getRepos(searchTerm) {
 
-  //   interface ApiResponse {
-  //    name: string;
-  //    description: string;
-  //    html_url: string;
+    interface ApiResponse {
+     name: string;
+     description: string;
+     html_url: string;
 
-  //   }
+    }
 
-  //   let searchPoint = 'https://api.github.com/users/' + searchTerm + '/repos?access_token=' + environment.GITAPIKEY;
-  //   let promise = new Promise((resolve, reject) => {
-  //     this.http.get<ApiResponse>(searchPoint).toPromise().then(
-  //       (repoResults) => {
-  //         console.log(repoResults);
-  //         this.repo = repoResults;
-  //         resolve();
-  //       },
-  //       (error) => {
-  //         console.log(error);
-  //         reject();
-  //       }
-  //     );
-  //   });
-  //   return promise;
+    let searchPoint = 'https://api.github.com/users/' + searchTerm + '/repos?access_token=' + environment.GITAPIKEY;
+    let promise = new Promise((resolve, reject) => {
+      this.http.get<ApiResponse>(searchPoint).toPromise().then(
+        (repoResults) => {
+          console.log(repoResults);
+          this.repository = repoResults;
+          resolve();
+        },
+        (error) => {
+          console.log(error);
+          reject();
+        }
+      );
+    });
+    return promise;
+}
 }
--- src/app/user.ts
+++ src/app/user.ts
@@ -1,5 +1,5 @@
 export class User {
-    constructor(public name: string, public username:string, public followers:number, public following: number, public repositories:number, public profile:string, public date: Date){
-        
+    constructor(public name: string, public username:string, public followers:number, public following: number, public repositories:number, public user, public profile:string, public date: Date){
+
     }
 }
