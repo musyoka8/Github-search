912b46158f17fcef1fb056240d8d38abd5034b1a
musyoka8 <nagilmusyoka@gmail.com>
2020-06-22 12:23:51 +0300

    add form component and what to display

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,13 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { HighlightDirective } from './highlight.directive';
 import { TimeCountPipe } from './time-count.pipe';
-import { UserComponent } from './User-profile/user/user.component';
 import { UserProfileComponent } from './user-profile/user-profile.component';
+import { Repository } from './repository';
 import { RepoProfileComponent } from './repo-profile/repo-profile.component';
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpClientModule } from '@ngx-progressbar/http-client';
+import { HomeComponent } from './home/home.component';
+import { SearchFormComponent } from './search-form/search-form.component';
 
 
 @NgModule({
@@ -18,14 +22,19 @@ import { RepoProfileComponent } from './repo-profile/repo-profile.component';
     LandingPageComponent,
     HighlightDirective,
     TimeCountPipe,
-    UserComponent,
     UserProfileComponent,
-    RepoProfileComponent
+    Repository,
+    RepoProfileComponent,
+    HomeComponent,
+    SearchFormComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    NgProgressModule.forRoot(),
+    NgProgressHttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
